17b500a77c8fd833b05b755e410030a3d39c0ced
Lacir Junior <lacir.junior@outlook.com>
2022-11-01 01:33:48 -0300

    Criação página envio de video e thumb

---
+++ DashboardPadrao/Icons/Icons.cs
@@ -0,0 +1,19 @@
+﻿using TabBlazor;
+
+namespace DashboardPadrao
+{
+    public static class Icons
+    {
+        public static string Home { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><polyline points='5 12 3 12 12 3 21 12 19 12' /><path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' /><path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' />"; }
+        public static string Bell { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6' /><path d='M9 17v1a3 3 0 0 0 6 0v-1' />"; }
+        public static string Search { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='10' cy='10' r='7' /><line x1='21' y1='21' x2='15' y2='15' />"; }
+        public static string Heart { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572' />"; }
+        public static string Upload { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2' /><polyline points='7 9 12 4 17 9' /><line x1='12' y1='4' x2='12' y2='16' />"; }
+        public static string Video { get => @"<path d='M18,14.5V11A1,1 0 0,0 17,10H16C18.24,8.39 18.76,5.27 17.15,3C15.54,0.78 12.42,0.26 10.17,1.87C9.5,2.35 8.96,3 8.6,3.73C6.25,2.28 3.17,3 1.72,5.37C0.28,7.72 1,10.8 3.36,12.25C3.57,12.37 3.78,12.5 4,12.58V21A1,1 0 0,0 5,22H17A1,1 0 0,0 18,21V17.5L22,21.5V10.5L18,14.5M13,4A2,2 0 0,1 15,6A2,2 0 0,1 13,8A2,2 0 0,1 11,6A2,2 0 0,1 13,4M6,6A2,2 0 0,1 8,8A2,2 0 0,1 6,10A2,2 0 0,1 4,8A2,2 0 0,1 6,6Z' />"; }
+        public static string Desktop_mac_dashboard { get => @"<path d='M21,14V4H3V14H21M21,2A2,2 0 0,1 23,4V16A2,2 0 0,1 21,18H14L16,21V22H8V21L10,18H3C1.89,18 1,17.1 1,16V4C1,2.89 1.89,2 3,2H21M4,5H15V10H4V5M16,5H20V7H16V5M20,8V13H16V8H20M4,11H9V13H4V11M10,11H15V13H10V11Z' />"; }
+        public static string Dashboard { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><circle cx='12' cy='13' r='2' /><line x1='13.45' y1='11.55' x2='15.5' y2='9.5' /><path d='M6.4 20a9 9 0 1 1 11.2 0z' />"; }
+        public static string Image { get => @"<path d='M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M6,20H15L18,20V12L14,16L12,14L6,20M8,9A2,2 0 0,0 6,11A2,2 0 0,0 8,13A2,2 0 0,0 10,11A2,2 0 0,0 8,9Z' />"; }
+        public static string Titulo { get => @"<path d='M5,4V7H10.5V19H13.5V7H19V4H5Z' />"; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto inicial usado a partir do template inicial do TabBlazor
+## [TabBlazor](https://github.com/joadan/TabBlazor)
